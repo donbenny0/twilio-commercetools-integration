2a0b1dbc092ac1c5b1e7e3f98bbdf9019420183f
don benny <don.benny@ayatacommerce.com>
2024-10-05 20:10:30 +0530

    fix : reconfigured jest package configuration

--- event/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var dotenv_1 = require("dotenv");
-var logger_utils_1 = require("./src/utils/logger.utils");
-dotenv_1.default.config();
-logger_utils_1.logger.info("env", process.env.CTP_REGION);
