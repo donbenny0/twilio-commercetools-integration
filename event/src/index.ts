9c9408c85787e042977f869d01af35cf655e60ee
don benny <don.benny@ayatacommerce.com>
2024-10-06 13:35:25 +0530

    debug : ENVIRONMENTS

--- event/src/index.ts
+++ event/src/index.ts
@@ -6,21 +6,29 @@ import app from './app';
 
 const PORT = 8080;
 
-// Sensitive variables you may want to mask or avoid logging
-const sensitiveKeys = ['API_KEY', 'DB_PASSWORD', 'JWT_SECRET', 'TWILIO_AUTH_TOKEN','CTP_CLIENT_SECRET','TWILIO_ACCOUNT_SID'];
-
-const maskSensitiveData = (key: string, value: string | undefined) => {
-  return sensitiveKeys.includes(key) ? '*****' : value;
-};
+// List of environment variables to retrieve and log
+const requiredEnvKeys = [
+  'CTP_CLIENT_ID',
+  'CTP_CLIENT_SECRET',
+  'CTP_PROJECT_KEY',
+  'CTP_SCOPE',
+  'CTP_REGION',
+  'TWILIO_ACCOUNT_SID',
+  'TWILIO_AUTH_TOKEN',
+  'TWILIO_FROM_NUMBER',
+  'CONNECT_PROVIDER',
+  'CONNECT_GCP_TOPIC_NAME',
+  'CONNECT_GCP_PROJECT_ID'
+];
 
 // Listen the application
 const server = app.listen(PORT, () => {
   logger.info(`⚡️ Event application listening on port ${PORT}`);
 
-  // Log environment variables
-  logger.info('Environment variables:');
-  Object.keys(process.env).forEach(key => {
-    logger.info(`${key}: ${maskSensitiveData(key, process.env[key])}`);
+  // Log only the specified environment variables
+  logger.info('Relevant environment variables:');
+  requiredEnvKeys.forEach(key => {
+    logger.info(`${key}: ${process.env[key] || 'Not defined'}`);
   });
 });
 
