ec3c32181515676375d40a8c76531e1a3dad6b7f
don benny <don.benny@ayatacommerce.com>
2024-10-06 12:48:45 +0530

    change : script changed from npm to yarn

--- event/src/index.ts
+++ event/src/index.ts
@@ -2,14 +2,26 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { logger } from './utils/logger.utils';
-
 import app from './app';
 
 const PORT = 8080;
 
+// Sensitive variables you may want to mask or avoid logging
+const sensitiveKeys = ['API_KEY', 'DB_PASSWORD', 'JWT_SECRET', 'TWILIO_AUTH_TOKEN','CTP_CLIENT_SECRET','TWILIO_ACCOUNT_SID'];
+
+const maskSensitiveData = (key: string, value: string | undefined) => {
+  return sensitiveKeys.includes(key) ? '*****' : value;
+};
+
 // Listen the application
 const server = app.listen(PORT, () => {
   logger.info(`⚡️ Event application listening on port ${PORT}`);
+
+  // Log environment variables
+  logger.info('Environment variables:');
+  Object.keys(process.env).forEach(key => {
+    logger.info(`${key}: ${maskSensitiveData(key, process.env[key])}`);
+  });
 });
 
 export default server;
