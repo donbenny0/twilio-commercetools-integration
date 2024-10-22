7f8fa33a45dc7cc73f8daa475ec7ca707adc402a
don benny <don.benny@ayatacommerce.com>
2024-10-22 13:40:21 +0530

    update : add service layer to transform order data

--- event/src/controllers/event.controller.ts
+++ event/src/controllers/event.controller.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
-import { getOrder } from '../repository/orders/getOrder';
 import { OrderInfo } from '../interfaces/order.interface';
 import sendWhatsAppMessage from '../utils/twilio.utils';
 import { decodePubSubData } from '../utils/helpers.utils';
 import { PubSubDecodedData } from '../interfaces/pubsub.interface';
 import CustomError from '../errors/custom.error';
+import { transformOrder } from '../services/orders/order.service';
 dotenv.config();
 
-// Controller to handle POST requests
+
 
 export const post = async (
   request: Request,
@@ -20,10 +20,10 @@ export const post = async (
     // Process Pub/Sub message
     const pubSubDecodedMessage: PubSubDecodedData = decodePubSubData(pubSubMessage);
     if (pubSubDecodedMessage.orderState !== "Confirmed") {
-      return response.status(400).send("Invalid orderState");
+      return response.status(400).send("Invalid order state");
     }
     // Fetch the order using commercetools
-    const order: OrderInfo = await getOrder(pubSubDecodedMessage.orderId);
+    const order: OrderInfo = await transformOrder(pubSubDecodedMessage.orderId);
 
     // Send WhatsApp messages
     await sendWhatsAppMessage(order);
--- event/src/repository/orders/getOrder.repository.ts
+++ event/src/repository/orders/getOrder.repository.ts
@@ -1,11 +1,5 @@
 import { createApiRoot } from '../../client/create.client';
-import { OrderInfo } from '../../interfaces/order.interface';
-import {
-  MissingOrderIdError,
-  OrderNotFoundError,
-  FetchOrderError,
-  InvalidOrderResponseError
-} from '../../errors/order.error'; // Assuming these errors are stored in 'order.errors.ts'
+import { OrderNotFoundError, FetchOrderError, InvalidOrderResponseError } from '../../errors/order.error';
 import CustomError from '../../errors/custom.error';
 
 // Create the API root for making requests to the commercetools platform
@@ -15,33 +9,20 @@ const apiRoot = createApiRoot();
  * Fetches an order using the provided order ID.
  * @param orderId - The ID of the order to retrieve.
  * @returns A promise that resolves to the order details if found.
- * @throws Will throw an error if the order ID is not provided or if the fetch operation fails.
  */
-export async function getOrder(orderId: string): Promise<OrderInfo> {
-  if (!orderId) {
-    throw new MissingOrderIdError();
-  }
+
+
+export async function getOrder(orderId: string) {
 
   try {
     // Fetch the order by ID
     const response = await apiRoot.orders().withId({ ID: orderId }).get().execute();
 
-    if (!response || !response.body) {
+    if (!response) {
       throw new InvalidOrderResponseError();
     }
 
-    const order = response.body;
-
-    if (!order.shippingAddress) {
-      throw new InvalidOrderResponseError();
-    }
-
-    const orderResponse: OrderInfo = {
-      shippingAddress: order.shippingAddress,
-    };
-
-    return orderResponse;
-
+    return response.body;
   } catch (error: any) {
     if (error.statusCode === 404) {
       throw new OrderNotFoundError(orderId);
