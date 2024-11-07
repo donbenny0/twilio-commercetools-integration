ae6bc192ae3d953ae5a95b25251d1c42f9ac96f0
don benny <don.benny@ayatacommerce.com>
2024-11-07 11:02:02 +0530

    debug : pubsub message

--- notify/src/controllers/event.controller.ts
+++ notify/src/controllers/event.controller.ts
@@ -5,10 +5,13 @@ import CustomError from '../errors/custom.error';
 import { addNotificationLog } from '../services/customObject/notifications/addNotificationLogs.service';
 import { messageHandler } from '../services/messaging/messageHandler.service';
 import { resourceHandler } from '../services/messaging/resourceHandler.service';
+import { logger } from '../utils/logger.utils';
 dotenv.config();
 
 
 export const post = async (request: Request, response: Response): Promise<Response | void> => {
+  logger.info('Received PubSub request body:', JSON.stringify(request.body));
+  logger.info('Received PubSub request message:', JSON.stringify(request.body.message));  
   const pubSubMessage = request.body.message;
   const pubSubDecodedMessage = decodePubSubData(pubSubMessage);
 
@@ -20,7 +23,6 @@ export const post = async (request: Request, response: Response): Promise<Respon
     await messageHandler(resourceData);
     await addNotificationLog('whatsapp', true, pubSubDecodedMessage);
     return response.status(200).send('Message sent successfully');
-
   } catch (error: any) {
     await addNotificationLog('whatsapp', false, pubSubDecodedMessage, error);
     return response.status(error instanceof CustomError ? error.statusCode as number : 500).send(error.message);
