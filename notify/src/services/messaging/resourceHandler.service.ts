29b6adbf7576dd850266e9efe0db99df541fe489
don benny <don.benny@ayatacommerce.com>
2024-11-19 11:19:55 +0530

    fix : deployment issue

--- notify/src/services/messaging/resourceHandler.service.ts
+++ notify/src/services/messaging/resourceHandler.service.ts
@@ -1,9 +1,10 @@
 import { transformOrder } from "../orders/orders.service";
 
 export const resourceHandler = async (decodedData: Record<string, any>) => {
-    if (decodedData.resource.typeId === 'order') {
+    if (decodedData.resource.typeId == 'order') {
+
         return await transformOrder(decodedData);
     }
-    
+
     return null;
 }
