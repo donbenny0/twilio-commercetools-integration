b0544296c13266be768fe7da8747e9b534e9bfe7
Don Benny <don.benny@ayatacommerce.com>
2025-03-21 11:44:05 +0530

    new ccommit

--- notify/src/routes/event.route.ts
+++ notify/src/routes/event.route.ts
@@ -7,8 +7,6 @@ const eventRouter: Router = Router();
 
 eventRouter.post('/', async (req, res, next) => {
   logger.info('Event message received');
-  logger.info('Event message request');
-  logger.info(JSON.stringify(req))
   logger.info('Event message body');
   logger.info(JSON.stringify(req.body))
   try {
